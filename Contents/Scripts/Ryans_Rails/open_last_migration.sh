4f67a1fc5e746caa8a32abeed7495e0da2103b2e
Ryan Wilcox <rwilcox@wilcoxd.com>
2012-10-10 11:00:02 -0400

    script that opens the most recently created Rails migration

---
+++ Contents/Scripts/Ryans_Rails/open_last_migration.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+BASE_FOLDER=`dirname "$0"`
+SUPPORT_FOLDER="$BASE_FOLDER/../../SharedSupport/"
+
+source "$SUPPORT_FOLDER/rails_path.sh"
+
+cd "$PROJECT_ROOT"
+/usr/local/bin/bbedit db/migrate/`ls -t1 db/migrate | head -n 1`
+
+
